52608ed18dceeab324ea4275cb37148a1abbbec0
aar1976871 <123432500+aar1976871@users.noreply.github.com>
2025-05-01 08:29:32 -0500

    added wrench.png

--- 2A/signon_page.php
+++ 2A/signon_page.php
@@ -3,6 +3,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/signon_page.css">
+        <link rel="icon" href="img/wrench.png" type="image/x-icon">
         <title>Login</title>
     </head>
     <body>
