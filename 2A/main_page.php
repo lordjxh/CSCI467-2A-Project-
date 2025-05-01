0f48aae160e219d26ad946922516d87eceaca1bf
aar1976871 <123432500+aar1976871@users.noreply.github.com>
2025-05-01 08:29:11 -0500

    added wrench.png

--- 2A/main_page.php
+++ 2A/main_page.php
@@ -22,6 +22,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="css/main_page.css">
+        <link rel="icon" href="img/wrench.png" type="image/x-icon">
         <title> Car Parts Catalog </title>
     </head>
     <body>
