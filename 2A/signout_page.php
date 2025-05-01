b58c727b235b62560115ec4f1d3182befaa170d1
aar1976871 <123432500+aar1976871@users.noreply.github.com>
2025-05-01 13:09:52 -0500

    Documented signout_page.php

--- 2A/signout_page.php
+++ 2A/signout_page.php
@@ -1,3 +1,8 @@
+<!-- 
+    Group 2A - CSCI 467 Spring 2025
+    signout_page.php - This page is created for logged in users to sign out
+        of their account. User is redirected to the sign on page
+-->
 <?php
     session_start();
     unset($_SESSION);
